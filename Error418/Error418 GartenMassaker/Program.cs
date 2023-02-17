b91f06e24ef87e80e1a3bf4240012f6041619638
Harry Röben <tiger.die.katze@gmail.com>
2023-02-17 21:07:16 +0100

    Now working

--- Error418/Error418 GartenMassaker/Program.cs
+++ Error418/Error418 GartenMassaker/Program.cs
@@ -1,70 +1,81 @@
 ﻿using Error418_GartenMassaker;
 using SocketIOClient;
 using System.Text.Json;
+using System.Text.Json.Nodes;
+using SocketIOClient.JsonSerializer;
 
-SocketIO socket = new("https://games.uhno.de", new SocketIOOptions {
-    Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+SocketIO socket = new("https://games.uhno.de", new SocketIOOptions
+{
+	Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
 });
-string secret = "b88f0722-18c7-4a92-9d61-4f1228a7bc39";
+string secret = "01bcf9dc-292c-4554-9ed2-00a65bd75553";
 
 int playerIndex = 0;
 
-socket.OnConnected += async (sender, e) => {
-    await socket.EmitAsync("authenticate", secret);
-    Console.WriteLine("Bot connected to Socket\n");
+socket.OnConnected += async (sender, e) =>
+{
+	await socket.EmitAsync("authenticate", secret);
+	Console.WriteLine("Bot connected to Socket\n");
 };
 
 await socket.ConnectAsync();
-socket.OnDisconnected += (sender, e) => {
-    Console.WriteLine("Disconnected" + sender.ToString() + e.ToString());
+socket.OnDisconnected += (sender, e) =>
+{
+	Console.WriteLine("Disconnected" + sender.ToString() + e.ToString());
 };
 
-socket.On("disconnect", data => {
-    Console.WriteLine(data);
+socket.On("disconnect", data =>
+{
+	Console.WriteLine(data);
 });
 
-socket.On("data", async response => {
-    Root test = response.GetValue<Root>();
-    Console.WriteLine(response);
-    switch (test.type) {
-        case "INIT":
-            Initialize(test);
-            break;
-        case "RESULT":
-            OnResulting(test);
-            break;
-        case "SET":
-            await SetBoard(test);
-            break;
-        case "ROUND":
-            await OnRound(test);
-            break;
-    }
+socket.On("data", async response =>
+{
+	Root test = response.GetValue<Root>();
+	Console.WriteLine(response);
+	switch (test.type)
+	{
+		case "INIT":
+			Initialize(test);
+			break;
+		case "RESULT":
+			OnResulting(test);
+			break;
+		case "SET":
+			await SetBoard(test, response);
+			break;
+		case "ROUND":
+			await OnRound(test);
+			break;
+	}
 
 });
 
-void Initialize(Root data) {
-    Console.WriteLine(data.type);
-    playerIndex = data.players[0].id == data.self ? 0 : 1;
+void Initialize(Root data)
+{
+	Console.WriteLine(data.type);
+	playerIndex = data.players[0].id == data.self ? 0 : 1;
 }
-void OnResulting(Root data) {
-    Console.WriteLine(data.type);
+void OnResulting(Root data)
+{
+	Console.WriteLine(data.type);
 }
-async Task SetBoard(Root data) {
-    Console.WriteLine(data.type);
-    Furniture[] allFurnitures = new Furniture[5] {
-        new Furniture { start = new int[] { 4, 3 }, direction = 'h', size = 5 },
-        new Furniture { start = new int[] { 8, 6 }, direction = 'v', size = 4 },
-        new Furniture { start = new int[] { 1, 5 }, direction = 'v', size = 3 },
-        new Furniture { start = new int[] { 3, 5 }, direction = 'v', size = 3 },
-        new Furniture { start = new int[] { 5,5 }, direction = 'v', size = 2 }
-    };
-    var packet = JsonSerializer.Serialize(allFurnitures);
-    await socket.EmitAsync("", packet);
+async Task SetBoard(Root data, SocketIOResponse response)
+{
+	Console.WriteLine(data.type);
+	Furniture[] allFurnitures = new Furniture[5] {
+		new Furniture { start = new int[] { 4, 3 }, Direction = 'h', Size = 5 },
+		new Furniture { start = new int[] { 8, 6 }, Direction = 'v', Size = 4 },
+		new Furniture { start = new int[] { 1, 5 }, Direction = 'v', Size = 3 },
+		new Furniture { start = new int[] { 3, 5 }, Direction = 'v', Size = 3 },
+		new Furniture { start = new int[] { 5,5 }, Direction = 'v', Size = 2 }
+	};
+
+	await response.CallbackAsync(allFurnitures.ToList());
 }
-async Task OnRound(Root data) {
-    Console.WriteLine(data.type);
+async Task OnRound(Root data)
+{
+	Console.WriteLine(data.type);
 }
 
 Console.ReadLine();
-
--- Error418/Error418 GartenMassaker/Response.cs
+++ Error418/Error418 GartenMassaker/Response.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Error418_GartenMassaker {
-    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
-    public class Player {
-        public string id { get; set; }
-        public int score { get; set; }
-    }
+namespace Error418_GartenMassaker
+{
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+	public class Player
+	{
+		public string id { get; set; }
+		public int score { get; set; }
+	}
 
-    public class Root {
-        public string id { get; set; }
-        public List<Player> players { get; set; }
-        public List<object> boards { get; set; }
-        public List<object> log { get; set; }
-        public string type { get; set; }
-        public string self { get; set; }
-    }
-    public class Furniture {
-        public int[] start { get; set; } = new int[2];
-        public char direction { get; set; }
-        public int size { get; set; }
-    }
+	public class Root
+	{
+		public string id { get; set; }
+		public List<Player> players { get; set; }
+		public List<object> boards { get; set; }
+		public List<object> log { get; set; }
+		public string type { get; set; }
+		public string self { get; set; }
+	}
+	public class Furniture
+	{
+		[JsonPropertyName("start")]
+		public int[] start { get; set; } = new int[2];
+		[JsonPropertyName("direction")]
+		public char Direction { get; set; }
+		[JsonPropertyName("size")]
+		public int Size { get; set; }
+	}
 }
