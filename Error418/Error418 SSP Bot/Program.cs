55272062edb28d5c9138a8ab8d44d1017f605d3e
Harry Röben <tiger.die.katze@gmail.com>
2023-02-17 13:54:09 +0100

    Added Working connection for bot

--- Error418/Error418 SSP Bot/Program.cs
+++ Error418/Error418 SSP Bot/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SocketIOClient;
+using System.Net.Sockets;
+
+SocketIO socket = new("https://games.uhno.de", new SocketIOOptions
+{
+	Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+});
+string secret = "177a7257-e8d9-4f9a-a5d6-1e6cc43743a4";
+
+socket.OnConnected += async (sender, e) =>
+{
+	await socket.EmitAsync("authenticate", secret);
+};
+
+await socket.ConnectAsync();
+
+socket.On("data", response =>
+{
+	Console.WriteLine(response);
+});
+
+
+Console.ReadLine();
